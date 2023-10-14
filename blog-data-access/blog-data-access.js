0ba4da4e40a2e300332d5e425ed7c5fe24cc32a7
Richard Gomez <gomezr3@students.westerntc.edu>
2023-10-14 12:41:53 -0700

    doen

---
+++ blog-data-access/blog-data-access.js
@@ -0,0 +1,18 @@
+const baseURL = "http://localhost:3000/";
+
+export function getAllPosts(){
+	return axios.get(baseURL + "pages/")
+		.then(resp => resp.data)
+		.catch(errorHandler);
+}
+
+export function getPostById(id){
+	return axios.get(baseURL + "pages/" + id)
+		.then(resp => resp.data)
+		.catch(errorHandler);
+}
+
+function errorHandler(err){
+    console.log("ERROR (in blog-data-access):", err.message);
+    throw new Error(err); // because we throw an error here, we can catch it in our components.
+}
